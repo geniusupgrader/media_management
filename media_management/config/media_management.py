58c9d304ed3809445673d14152921b13286bfdf1
Robin Rosenstock <robin.rosenstock@t-online.de>
2020-05-11 10:11:30 +0200

    - added „Movie“ Doctype - added module card in home

---
+++ media_management/config/media_management.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+    return [
+      {
+        "label":_("Media Management"),
+        "icon": "octicon octicon-briefcase",
+        "items": [
+                    {
+              "type": "doctype",
+              "name": "Movie",
+              "label": _("Movie"),
+              "description": _("Description of Liste"),
+            }
+        ]
+      }
+  ]
