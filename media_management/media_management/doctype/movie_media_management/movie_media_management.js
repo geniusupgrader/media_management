5723c9dc8f95b3e3089385bc8c64c24f2c9ae2cb
Robin Rosenstock <robin.rosenstock@t-online.de>
2020-06-11 11:49:31 +0200

    renamed doctypes and other things

---
+++ media_management/media_management/doctype/movie_media_management/movie_media_management.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Robin Rosenstock and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Movie_media_management_media_management', {
+	// refresh: function(frm) {
+
+	// }
+});
