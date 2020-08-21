1e5b58de55b20a49dcdd0ea48a3938bd703b6465
Robin Rosenstock <robin.rosenstock@t-online.de>
2020-08-21 23:04:21 +0200

    doctype link updates 2

---
+++ media_management/media_management/doctype/science_field_media_management/test_science_field_media_management.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Robin Rosenstock and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestScience_field_media_management(unittest.TestCase):
+	pass
