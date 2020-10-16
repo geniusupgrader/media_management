7d9bddfa0aba1770d2dc31f6bf6b273a9603ea8f
Robin Rosenstock <robin.rosenstock@t-online.de>
2020-10-16 19:06:51 +0200

    website link custom script and changes in doctype

--- media_management/media_management/doctype/website_link_media_management/website_link_media_management.js
+++ media_management/media_management/doctype/website_link_media_management/website_link_media_management.js
@@ -2,7 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Website_Link_media_management', {
-	// refresh: function(frm) {
+	refresh(frm) {
+		frm.add_custom_button(__("Open Link"), function() {
+		window.open(frm.doc.link);
+	});
+	frm.doc.link_read_only = frm.doc.link
+	}
+})
 
-	// }
-});
--- media_management/media_management/doctype/website_link_media_management/website_link_media_management.py
+++ media_management/media_management/doctype/website_link_media_management/website_link_media_management.py
@@ -5,6 +5,8 @@
 from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
+# from frappe.utils.jinja import render_template
+
 
 class Website_Link_media_management(Document):
 	pass
