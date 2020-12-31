8dcc3bdf924a672aaef6a50c782dae08f9ea5293
Robin Rosenstock <robin.rosenstock@t-online.de>
2020-12-31 23:17:25 +0100

    changed module icon and read only turned from the new ones

--- media_management/config/desktop.py
+++ media_management/config/desktop.py
@@ -7,7 +7,7 @@ def get_data():
 		{
 			"module_name": "Media Management",
 			"color": "grey",
-			"icon": "octicon octicon-file-directory",
+			"icon": "octicon octicon-device-desktop",
 			"type": "module",
 			"label": _("Media Management")
 		}
